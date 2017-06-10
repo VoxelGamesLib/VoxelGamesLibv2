9db3f72324f535aa30fc6454c968b5f8e961503d
MiniDigger <admin@minidigger.me>
2017-06-10 10:40:26 +0200

    add bstats

---
+++ src/main/java/me/minidigger/voxelgameslib/MetricHandler.java
@@ -0,0 +1,32 @@
+package me.minidigger.voxelgameslib;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import lombok.extern.java.Log;
+import me.minidigger.voxelgameslib.handler.Handler;
+import org.bstats.Metrics;
+
+@Log
+@Singleton
+public class MetricHandler implements Handler {
+
+  private Metrics metrics;
+
+  @Inject
+  private VoxelGamesLib voxelGamesLib;
+
+  @Override
+  public void start() {
+    try {
+      metrics = new Metrics(voxelGamesLib);
+      //TODO add custom charts, like user/gamemode, installed gamesmodes, user/lang, installed langs etc
+    } catch (Exception ex) {
+      log.warning("Metrics failed to enabled. This is not a critical problem. You can ignore it.");
+    }
+  }
+
+  @Override
+  public void stop() {
+    metrics = null;
+  }
+}
