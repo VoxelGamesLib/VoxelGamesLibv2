505d2160dff612c41ee4e272ead3419e3ebea47f
MiniDigger <admin@minidigger.me>
2017-07-04 22:22:49 +0200

    just fuck you jgit

--- src/main/java/me/minidigger/voxelgameslib/world/WorldRepository.java
+++ src/main/java/me/minidigger/voxelgameslib/world/WorldRepository.java
@@ -4,8 +4,13 @@ import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import javax.inject.Inject;
 import javax.inject.Named;
 
@@ -52,11 +57,24 @@ public class WorldRepository {
     public void commitRepo() {
         try {
             Git git = Git.open(worldsDir);
-            git.add().addFilepattern(".").call();
+            new CustomAddCommand(git.getRepository()).addFilepattern(".").call();
             git.commit().setAuthor("voxelgameslib", "voxelgameslib@minidigger.me")
                     .setMessage("Update " + LocalDateTime.now().toString()).call();
             // don't push here, thats need to be done manually
         } catch (IOException | GitAPIException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void fixDaFuckingStupidGitignore() {
+        File[] files = worldsDir.listFiles();
+        if (files == null) return;
+        File gitignore = new File(worldsDir, ".gitignore");
+        try (PrintWriter writer = new PrintWriter(new FileWriter(gitignore, false))) {
+            for (String name : Arrays.stream(files).map(File::getName).filter(name -> name.endsWith(".zip")).collect(Collectors.toList())) {
+                writer.println("!" + name);
+            }
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
