72b22809d86d935d7bbf9b0bc874325317b72be2
aphel <aphel@indices.io>
2017-07-08 18:54:10 +0100

    teleport player to spawn if they fall in void feature

---
+++ src/main/java/me/minidigger/voxelgameslib/feature/features/VoidTeleportFeature.java
@@ -0,0 +1,62 @@
+package me.minidigger.voxelgameslib.feature.features;
+
+import me.minidigger.voxelgameslib.feature.AbstractFeature;
+import me.minidigger.voxelgameslib.feature.FeatureInfo;
+
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+
+@FeatureInfo(name = "VoidTeleportFeature", author = "aphel", version = "1.0",
+        description = "Teleports player to spawn if they fall into the void")
+public class VoidTeleportFeature extends AbstractFeature {
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public void tick() {
+
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public Class[] getDependencies() {
+        return new Class[0];
+    }
+
+    @EventHandler
+    public void onVoidDamage(EntityDamageEvent event) {
+        if(event.getEntityType() != EntityType.PLAYER)
+            return;
+
+        if(event.getCause().equals(EntityDamageEvent.DamageCause.VOID) && getPhase().getGame().isPlaying(event.getEntity().getUniqueId())) {
+            Player player = (Player) event.getEntity();
+
+            player.teleport(player.getWorld().getSpawnLocation());
+
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onFellOutOfWorld(PlayerMoveEvent event) {
+        // just in case damage is disabled
+        if(event.getTo().getY() < 0 && getPhase().getGame().isPlaying(event.getPlayer().getUniqueId())) {
+            event.getPlayer().teleport(event.getPlayer().getWorld().getSpawnLocation());
+        }
+    }
+}
