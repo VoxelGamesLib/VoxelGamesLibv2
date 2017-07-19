926af554d176c14e8df694ec0b3532fda1e0f268
MiniDigger <admin@minidigger.me>
2017-07-19 19:00:40 +0200

    small draft for inventories

--- src/main/java/me/minidigger/voxelgameslib/components/inventory/Inventory.java
+++ src/main/java/me/minidigger/voxelgameslib/components/inventory/Inventory.java
@@ -1,14 +1,23 @@
 package me.minidigger.voxelgameslib.components.inventory;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.BiConsumer;
 
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.ItemStack;
 
-interface Inventory {
+public class Inventory {
 
-    /**
-     * Acts when an inventory is clicked
-     */
-    void onClick(BiConsumer<ItemStack, InventoryClickEvent> action);
+    private Map<ItemStack, BiConsumer<ItemStack, InventoryClickEvent>> actions = new HashMap<>();
+
+    public void addAction(ItemStack is, BiConsumer<ItemStack, InventoryClickEvent> action) {
+        actions.put(is, action);
+    }
+
+    public void onClick(ItemStack is, InventoryClickEvent e) {
+        if (actions.containsKey(is)) {
+            actions.get(is).accept(is, e);
+        }
+    }
 }
--- src/main/java/me/minidigger/voxelgameslib/components/inventory/InventoryHandler.java
+++ src/main/java/me/minidigger/voxelgameslib/components/inventory/InventoryHandler.java
@@ -7,8 +7,11 @@ import javax.inject.Inject;
 
 import me.minidigger.voxelgameslib.VoxelGamesLib;
 import me.minidigger.voxelgameslib.handler.Handler;
+import me.minidigger.voxelgameslib.utils.ItemBuilder;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
@@ -23,6 +26,10 @@ public class InventoryHandler implements Handler, Listener {
     @Override
     public void start() {
         Bukkit.getPluginManager().registerEvents(this, voxelGamesLib);
+
+        Inventory inventory = new Inventory();
+        inventory.addAction(new ItemBuilder(Material.MAGMA_CREAM).name("Click me").build(),
+                (is, event) -> event.getWhoClicked().sendMessage("You clicked " + is.getItemMeta().getDisplayName()));
     }
 
     @Override
@@ -30,7 +37,10 @@ public class InventoryHandler implements Handler, Listener {
         HandlerList.unregisterAll(this);
     }
 
+    @EventHandler
     public void onClickListener(InventoryClickEvent event) {
         // todo
+        Inventory inv = null; // todo get the inv
+        inv.onClick(event.getCurrentItem(), event);
     }
 }
