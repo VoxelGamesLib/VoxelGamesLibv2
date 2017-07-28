0655cc17f9ae46f2d7238d2e51256ff9ed6f8ea6
MiniDigger <admin@minidigger.me>
2017-07-28 18:11:08 +0200

    add a multiline chart to bstats

--- src/main/java/me/minidigger/voxelgameslib/metrics/MetricHandler.java
+++ src/main/java/me/minidigger/voxelgameslib/metrics/MetricHandler.java
@@ -2,10 +2,13 @@ package me.minidigger.voxelgameslib.metrics;
 
 import org.bstats.bukkit.Metrics;
 
+import java.util.HashMap;
+import java.util.Map;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
 import me.minidigger.voxelgameslib.VoxelGamesLib;
+import me.minidigger.voxelgameslib.game.GameHandler;
 import me.minidigger.voxelgameslib.handler.Handler;
 
 import lombok.extern.java.Log;
@@ -18,12 +21,21 @@ public class MetricHandler implements Handler {
 
     @Inject
     private VoxelGamesLib voxelGamesLib;
+    @Inject
+    private GameHandler gameHandler;
 
     @Override
     public void start() {
         try {
             metrics = new Metrics(voxelGamesLib);
             //TODO add custom charts, like user/gamemode, installed gamesmodes, user/lang, installed langs etc
+
+            // gamemodes multiline TODO enable this on the bstats page once its implemented....
+            metrics.addCustomChart(new Metrics.MultiLineChart("gamemodes", () -> {
+                Map<String, Integer> valueMap = new HashMap<>();
+                gameHandler.getGameModes().forEach((gm) -> valueMap.put(gm.getName(), 1));
+                return valueMap;
+            }));
         } catch (Throwable ex) {
             log.warning("Metrics failed to enabled. This is not a critical problem. You can ignore it.");
         }
