08b322505c2caf39521b3a06f5deb3dccd3eda39
kashike <kashike@vq.lc>
2017-08-12 12:18:01 -0700

    Improve sanity of EntityUtil

--- src/main/java/com/voxelgameslib/voxelgameslib/utils/EntityUtil.java
+++ src/main/java/com/voxelgameslib/voxelgameslib/utils/EntityUtil.java
@@ -1,301 +1,124 @@
 package com.voxelgameslib.voxelgameslib.utils;
 
-import org.apache.commons.lang.ArrayUtils;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.Arrays;
+import java.util.Map;
 import javax.annotation.Nonnull;
 
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Multimap;
 import org.bukkit.entity.EntityType;
 
 public class EntityUtil {
 
-
-    private static HashMap<EntityType, Type> entitys = new HashMap<>();
+    private static final Map<EntityType, Type> ENTITY_TYPE_TO_TYPE = ImmutableMap.<EntityType, Type>builder()
+        .put(EntityType.ARROW, Type.PROJECTILE)
+        .put(EntityType.BAT, Type.PASSIVE)
+        .put(EntityType.BLAZE, Type.ANGRY)
+        .put(EntityType.BOAT, Type.OTHER)
+        .put(EntityType.CAVE_SPIDER, Type.PASSIVE)
+        .put(EntityType.CHICKEN, Type.FRIENDLY)
+        .put(EntityType.COMPLEX_PART, Type.OTHER)
+        .put(EntityType.COW, Type.FRIENDLY)
+        .put(EntityType.CREEPER, Type.FRIENDLY)
+        .put(EntityType.DROPPED_ITEM, Type.OTHER)
+        .put(EntityType.AREA_EFFECT_CLOUD, Type.UTILITY)
+        .put(EntityType.ELDER_GUARDIAN, Type.ANGRY)
+        .put(EntityType.WITHER_SKELETON, Type.ANGRY)
+        .put(EntityType.STRAY, Type.ANGRY)
+        .put(EntityType.EGG, Type.OTHER)
+        .put(EntityType.ENDERMAN, Type.PASSIVE)
+        .put(EntityType.ENDER_CRYSTAL, Type.OTHER)
+        .put(EntityType.ENDER_DRAGON, Type.ANGRY)
+        .put(EntityType.ENDER_PEARL, Type.OTHER)
+        .put(EntityType.ENDER_SIGNAL, Type.OTHER)
+        .put(EntityType.EXPERIENCE_ORB, Type.OTHER)
+        .put(EntityType.FALLING_BLOCK, Type.OTHER)
+        .put(EntityType.FIREBALL, Type.PROJECTILE)
+        .put(EntityType.FIREWORK, Type.OTHER)
+        .put(EntityType.FISHING_HOOK, Type.PROJECTILE)
+        .put(EntityType.GHAST, Type.ANGRY)
+        .put(EntityType.GIANT, Type.ANGRY)
+        .put(EntityType.HORSE, Type.FRIENDLY)
+        .put(EntityType.IRON_GOLEM, Type.UTILITY)
+        .put(EntityType.ITEM_FRAME, Type.OTHER)
+        .put(EntityType.LEASH_HITCH, Type.OTHER)
+        .put(EntityType.LIGHTNING, Type.OTHER)
+        .put(EntityType.MAGMA_CUBE, Type.ANGRY)
+        .put(EntityType.MINECART, Type.CART)
+        .put(EntityType.MINECART_CHEST, Type.CART)
+        .put(EntityType.MINECART_COMMAND, Type.CART)
+        .put(EntityType.MINECART_FURNACE, Type.CART)
+        .put(EntityType.MINECART_HOPPER, Type.CART)
+        .put(EntityType.MINECART_MOB_SPAWNER, Type.CART)
+        .put(EntityType.MINECART_TNT, Type.CART)
+        .put(EntityType.MUSHROOM_COW, Type.FRIENDLY)
+        .put(EntityType.OCELOT, Type.FRIENDLY)
+        .put(EntityType.PAINTING, Type.OTHER)
+        .put(EntityType.PIG, Type.FRIENDLY)
+        .put(EntityType.PIG_ZOMBIE, Type.PASSIVE)
+        .put(EntityType.PLAYER, Type.OTHER)
+        .put(EntityType.PRIMED_TNT, Type.OTHER)
+        .put(EntityType.SHEEP, Type.FRIENDLY)
+        .put(EntityType.SILVERFISH, Type.ANGRY)
+        .put(EntityType.SKELETON, Type.ANGRY)
+        .put(EntityType.SLIME, Type.ANGRY)
+        .put(EntityType.SMALL_FIREBALL, Type.PROJECTILE)
+        .put(EntityType.SNOWBALL, Type.PROJECTILE)
+        .put(EntityType.SNOWMAN, Type.UTILITY)
+        .put(EntityType.SPIDER, Type.PASSIVE)
+        .put(EntityType.SPLASH_POTION, Type.OTHER)
+        .put(EntityType.SQUID, Type.FRIENDLY)
+        .put(EntityType.THROWN_EXP_BOTTLE, Type.OTHER)
+        .put(EntityType.UNKNOWN, Type.OTHER)
+        .put(EntityType.VILLAGER, Type.FRIENDLY)
+        .put(EntityType.WEATHER, Type.OTHER)
+        .put(EntityType.WITCH, Type.ANGRY)
+        .put(EntityType.WITHER, Type.ANGRY)
+        .put(EntityType.WITHER_SKULL, Type.PROJECTILE)
+        .put(EntityType.WOLF, Type.PASSIVE)
+        .put(EntityType.ZOMBIE, Type.ANGRY)
+        .put(EntityType.HUSK, Type.ANGRY)
+        .put(EntityType.SPECTRAL_ARROW, Type.PROJECTILE)
+        .put(EntityType.SHULKER_BULLET, Type.PROJECTILE)
+        .put(EntityType.DRAGON_FIREBALL, Type.PROJECTILE)
+        .put(EntityType.ZOMBIE_VILLAGER, Type.ANGRY)
+        .put(EntityType.SKELETON_HORSE, Type.ANGRY)
+        .put(EntityType.ZOMBIE_HORSE, Type.ANGRY)
+        .put(EntityType.ARMOR_STAND, Type.UTILITY)
+        .put(EntityType.ENDERMITE, Type.ANGRY)
+        .put(EntityType.GUARDIAN, Type.ANGRY)
+        .put(EntityType.RABBIT, Type.FRIENDLY)
+        .put(EntityType.DONKEY, Type.FRIENDLY)
+        .put(EntityType.MULE, Type.FRIENDLY)
+        .put(EntityType.EVOKER_FANGS, Type.PROJECTILE)
+        .put(EntityType.EVOKER, Type.ANGRY)
+        .put(EntityType.VEX, Type.ANGRY)
+        .put(EntityType.VINDICATOR, Type.ANGRY)
+        .put(EntityType.ILLUSIONER, Type.ANGRY)
+        .put(EntityType.SHULKER, Type.ANGRY)
+        .put(EntityType.POLAR_BEAR, Type.FRIENDLY)
+        .put(EntityType.LLAMA, Type.FRIENDLY)
+        .put(EntityType.LLAMA_SPIT, Type.PROJECTILE)
+        .put(EntityType.PARROT, Type.FRIENDLY)
+        .put(EntityType.LINGERING_POTION, Type.UTILITY)
+        .put(EntityType.TIPPED_ARROW, Type.PROJECTILE)
+        .build();
+    private static final Multimap<Type, EntityType> TYPE_TO_ENTITY_TYPES = HashMultimap.create();
 
     static {
         for (final EntityType type : EntityType.values()) {
-            switch (type) {
-                case ARROW:
-                    entitys.put(type, Type.PROJECTILE);
-                    break;
-                case BAT:
-                    entitys.put(type, Type.PASSIV);
-                    break;
-                case BLAZE:
-                    entitys.put(type, Type.ANGRY);
-                    break;
-                case BOAT:
-                    entitys.put(type, Type.OTHER);
-                    break;
-                case CAVE_SPIDER:
-                    entitys.put(type, Type.PASSIV);
-                    break;
-                case CHICKEN:
-                    entitys.put(type, Type.FRIENDLY);
-                    break;
-                case COMPLEX_PART:
-                    entitys.put(type, Type.OTHER);
-                    break;
-                case COW:
-                    entitys.put(type, Type.FRIENDLY);
-                    break;
-                case CREEPER:
-                    entitys.put(type, Type.FRIENDLY);
-                    break;
-                case DROPPED_ITEM:
-                    entitys.put(type, Type.OTHER);
-                    break;
-                case AREA_EFFECT_CLOUD:
-                    entitys.put(type, Type.UTILITY);
-                    break;
-                case ELDER_GUARDIAN:
-                    entitys.put(type, Type.ANGRY);
-                    break;
-                case WITHER_SKELETON:
-                    entitys.put(type, Type.ANGRY);
-                    break;
-                case STRAY:
-                    entitys.put(type, Type.ANGRY);
-                    break;
-                case EGG:
-                    entitys.put(type, Type.OTHER);
-                    break;
-                case ENDERMAN:
-                    entitys.put(type, Type.PASSIV);
-                    break;
-                case ENDER_CRYSTAL:
-                    entitys.put(type, Type.OTHER);
-                    break;
-                case ENDER_DRAGON:
-                    entitys.put(type, Type.ANGRY);
-                    break;
-                case ENDER_PEARL:
-                    entitys.put(type, Type.OTHER);
-                    break;
-                case ENDER_SIGNAL:
-                    entitys.put(type, Type.OTHER);
-                    break;
-                case EXPERIENCE_ORB:
-                    entitys.put(type, Type.OTHER);
-                    break;
-                case FALLING_BLOCK:
-                    entitys.put(type, Type.OTHER);
-                    break;
-                case FIREBALL:
-                    entitys.put(type, Type.PROJECTILE);
-                    break;
-                case FIREWORK:
-                    entitys.put(type, Type.OTHER);
-                    break;
-                case FISHING_HOOK:
-                    entitys.put(type, Type.PROJECTILE);
-                    break;
-                case GHAST:
-                    entitys.put(type, Type.ANGRY);
-                    break;
-                case GIANT:
-                    entitys.put(type, Type.ANGRY);
-                    break;
-                case HORSE:
-                    entitys.put(type, Type.FRIENDLY);
-                    break;
-                case IRON_GOLEM:
-                    entitys.put(type, Type.UTILITY);
-                    break;
-                case ITEM_FRAME:
-                    entitys.put(type, Type.OTHER);
-                    break;
-                case LEASH_HITCH:
-                    entitys.put(type, Type.OTHER);
-                    break;
-                case LIGHTNING:
-                    entitys.put(type, Type.OTHER);
-                    break;
-                case MAGMA_CUBE:
-                    entitys.put(type, Type.ANGRY);
-                    break;
-                case MINECART:
-                    entitys.put(type, Type.CART);
-                    break;
-                case MINECART_CHEST:
-                    entitys.put(type, Type.CART);
-                    break;
-                case MINECART_COMMAND:
-                    entitys.put(type, Type.CART);
-                    break;
-                case MINECART_FURNACE:
-                    entitys.put(type, Type.CART);
-                    break;
-                case MINECART_HOPPER:
-                    entitys.put(type, Type.CART);
-                    break;
-                case MINECART_MOB_SPAWNER:
-                    entitys.put(type, Type.CART);
-                    break;
-                case MINECART_TNT:
-                    entitys.put(type, Type.CART);
-                    break;
-                case MUSHROOM_COW:
-                    entitys.put(type, Type.FRIENDLY);
-                    break;
-                case OCELOT:
-                    entitys.put(type, Type.FRIENDLY);
-                    break;
-                case PAINTING:
-                    entitys.put(type, Type.OTHER);
-                    break;
-                case PIG:
-                    entitys.put(type, Type.FRIENDLY);
-                    break;
-                case PIG_ZOMBIE:
-                    entitys.put(type, Type.PASSIV);
-                    break;
-                case PLAYER:
-                    entitys.put(type, Type.OTHER);
-                    break;
-                case PRIMED_TNT:
-                    entitys.put(type, Type.OTHER);
-                    break;
-                case SHEEP:
-                    entitys.put(type, Type.FRIENDLY);
-                    break;
-                case SILVERFISH:
-                    entitys.put(type, Type.ANGRY);
-                    break;
-                case SKELETON:
-                    entitys.put(type, Type.ANGRY);
-                    break;
-                case SLIME:
-                    entitys.put(type, Type.ANGRY);
-                    break;
-                case SMALL_FIREBALL:
-                    entitys.put(type, Type.PROJECTILE);
-                    break;
-                case SNOWBALL:
-                    entitys.put(type, Type.PROJECTILE);
-                    break;
-                case SNOWMAN:
-                    entitys.put(type, Type.UTILITY);
-                    break;
-                case SPIDER:
-                    entitys.put(type, Type.PASSIV);
-                    break;
-                case SPLASH_POTION:
-                    entitys.put(type, Type.OTHER);
-                    break;
-                case SQUID:
-                    entitys.put(type, Type.FRIENDLY);
-                    break;
-                case THROWN_EXP_BOTTLE:
-                    entitys.put(type, Type.OTHER);
-                    break;
-                case UNKNOWN:
-                    entitys.put(type, Type.OTHER);
-                    break;
-                case VILLAGER:
-                    entitys.put(type, Type.FRIENDLY);
-                    break;
-                case WEATHER:
-                    entitys.put(type, Type.OTHER);
-                    break;
-                case WITCH:
-                    entitys.put(type, Type.ANGRY);
-                    break;
-                case WITHER:
-                    entitys.put(type, Type.ANGRY);
-                    break;
-                case WITHER_SKULL:
-                    entitys.put(type, Type.PROJECTILE);
-                    break;
-                case WOLF:
-                    entitys.put(type, Type.PASSIV);
-                    break;
-                case ZOMBIE:
-                    entitys.put(type, Type.ANGRY);
-                    break;
-                case HUSK:
-                    entitys.put(type, Type.ANGRY);
-                    break;
-                case SPECTRAL_ARROW:
-                    entitys.put(type, Type.PROJECTILE);
-                    break;
-                case SHULKER_BULLET:
-                    entitys.put(type, Type.PROJECTILE);
-                    break;
-                case DRAGON_FIREBALL:
-                    entitys.put(type, Type.PROJECTILE);
-                    break;
-                case ZOMBIE_VILLAGER:
-                    entitys.put(type, Type.ANGRY);
-                    break;
-                case SKELETON_HORSE:
-                    entitys.put(type, Type.ANGRY);
-                    break;
-                case ZOMBIE_HORSE:
-                    entitys.put(type, Type.ANGRY);
-                    break;
-                case ARMOR_STAND:
-                    entitys.put(type, Type.UTILITY);
-                    break;
-                case ENDERMITE:
-                    entitys.put(type, Type.ANGRY);
-                    break;
-                case GUARDIAN:
-                    entitys.put(type, Type.ANGRY);
-                    break;
-                case RABBIT:
-                    entitys.put(type, Type.FRIENDLY);
-                    break;
-                case DONKEY:
-                    entitys.put(type, Type.FRIENDLY);
-                    break;
-                case MULE:
-                    entitys.put(type, Type.FRIENDLY);
-                    break;
-                case EVOKER_FANGS:
-                    entitys.put(type, Type.PROJECTILE);
-                    break;
-                case EVOKER:
-                    entitys.put(type, Type.ANGRY);
-                    break;
-                case VEX:
-                    entitys.put(type, Type.ANGRY);
-                    break;
-                case VINDICATOR:
-                    entitys.put(type, Type.ANGRY);
-                    break;
-                case ILLUSIONER:
-                    entitys.put(type, Type.ANGRY);
-                    break;
-                case SHULKER:
-                    entitys.put(type, Type.ANGRY);
-                    break;
-                case POLAR_BEAR:
-                    entitys.put(type, Type.FRIENDLY);
-                    break;
-                case LLAMA:
-                    entitys.put(type, Type.FRIENDLY);
-                    break;
-                case LLAMA_SPIT:
-                    entitys.put(type, Type.PROJECTILE);
-                    break;
-                case PARROT:
-                    entitys.put(type, Type.FRIENDLY);
-                    break;
-                case LINGERING_POTION:
-                    entitys.put(type, Type.UTILITY);
-                    break;
-                case TIPPED_ARROW:
-                    entitys.put(type, Type.PROJECTILE);
-                    break;
+            if (!ENTITY_TYPE_TO_TYPE.containsKey(type)) {
+                throw new IllegalStateException("Entity type '" + type + "' has not been mapped");
             }
+        }
+        for (final Map.Entry<EntityType, Type> entry : ENTITY_TYPE_TO_TYPE.entrySet()) {
+            TYPE_TO_ENTITY_TYPES.put(entry.getValue(), entry.getKey());
         }
     }
 
     public enum Type {
-        PASSIV,
+        PASSIVE,
         ANGRY,
         FRIENDLY,
         PROJECTILE,
@@ -305,15 +128,7 @@ public class EntityUtil {
     }
 
     @Nonnull
-    public static EntityType[] getAll(@Nonnull Type... types) {
-        List<EntityType> result = new ArrayList<>();
-
-        for (EntityType type : entitys.keySet()) {
-            if (ArrayUtils.contains(types, entitys.get(type))) {
-                result.add(type);
-            }
-        }
-
-        return result.toArray(new EntityType[result.size()]);
+    public static EntityType[] getAll(@Nonnull final Type... types) {
+        return Arrays.stream(types).map(TYPE_TO_ENTITY_TYPES::get).toArray(EntityType[]::new);
     }
 }
