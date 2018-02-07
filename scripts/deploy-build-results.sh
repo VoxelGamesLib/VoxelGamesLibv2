3484be3ebeacb8208e9211834493b6f8f2b963da
MiniDigger <admin@minidigger.me>
2018-02-07 21:30:31 +0100

    try a bit more output

--- scripts/deploy-build-results.sh
+++ scripts/deploy-build-results.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
+echo "clean deploy filder"
 rm -rf deploy-stuff
 mkdir deploy-stuff
 
 # config
+echo "setup git"
 git config --global user.email "nobody@nobody.org"
 git config --global user.name "Travis CI"
 
 # copy over stuff we want to deploy
+echo "copy stuff to deploy"
 cp -R build/dependencyUpdates/. deploy-stuff/
 
 # deploy
+echo "create repo"
 cd deploy-stuff
 git init
 git add .
+echo "commit"
 git commit -m "Deploy to Github Pages"
+echo "push"
 git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/VoxelGamesLib/VoxelGamesLibv2.git" master:gh-pages > /dev/null 2>&1
