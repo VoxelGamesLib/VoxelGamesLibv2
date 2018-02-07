b742ceef9c104b7e82afde1eef157f3c6db54c93
MiniDigger <mini.digger@live.de>
2018-02-07 16:54:04 +0100

    add script to deploy stuff to github

---
+++ scripts/deploy-build-results.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -o errexit
+
+rm -rf deploy-stuff
+mkdir deploy-stuff
+
+# config
+git config --global user.email "nobody@nobody.org"
+git config --global user.name "Travis CI"
+
+# copy over stuff we want to deploy
+cp -R build/dependencyUpdates/. deploy-stuff/
+
+# deploy
+cd deploy-stuff
+git init
+git add .
+git commit -m "Deploy to Github Pages"
+git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/VoxelGamesLib/VoxelGamesLibv2.git" master:gh-pages > /dev/null 2>&1
