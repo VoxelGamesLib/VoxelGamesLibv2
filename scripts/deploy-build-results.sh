7fd0df8e003448633ae8d27f40050213d954c854
MiniDigger <admin@minidigger.me>
2018-02-11 15:47:51 +0100

    install mako

--- scripts/deploy-build-results.sh
+++ scripts/deploy-build-results.sh
@@ -21,6 +21,7 @@ mkdir deploy-stuff/mvn-repo/
 mvn deploy:deploy-file -Dfile=build/libs/voxelgameslib-2.0-SNAPSHOT.jar -DpomFile=pom.xml  -Durl=file://${TRAVIS_BUILD_DIR}/deploy-stuff/mvn-repo
 
 # create index
+pip install mako
 python scripts/make_index.py --header "VGL Deployments"
 
 # deploy
