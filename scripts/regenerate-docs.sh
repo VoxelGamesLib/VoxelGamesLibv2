737a9027510c1b0a2eb51d6d4691d517dd778766
MiniDigger <admin@minidigger.me>
2018-02-19 22:25:53 +0100

    use shadow url

--- scripts/regenerate-docs.sh
+++ scripts/regenerate-docs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo 'wget cli.jar'
-wget https://github.com/VoxelGamesLib/VoxelGamesLibv2/raw/gh-pages/commandline-tools/tools/commandline-tools-1.0-SNAPSHOT.jar -O cli.jar
+wget https://github.com/VoxelGamesLib/VoxelGamesLibv2/blob/gh-pages/commandline-tools/tools/commandline-tools-1.0-SNAPSHOT-all.jar?raw=true -O cli.jar
 echo 'run cli.jar'
 java -jar cli.jar -generateDocs -docsFolder docs
 echo 'push'
