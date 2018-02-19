c502d0df42d32de5e77d4fc15a75808938a9ae48
MiniDigger <admin@minidigger.me>
2018-02-19 21:54:16 +0100

    temp disable cli docs

--- scripts/regenerate-docs.sh
+++ scripts/regenerate-docs.sh
@@ -1,7 +1,8 @@
+#!/usr/bin/env bash
 echo 'wget cli.jar'
 wget https://github.com/VoxelGamesLib/VoxelGamesLibv2/raw/gh-pages/commandline-tools/tools/commandline-tools-1.0-SNAPSHOT.jar -O cli.jar
 echo 'run cli.jar'
-java -jar cli.jar -generateDocs -docsFolder docs
+# java -jar cli.jar -generateDocs -docsFolder docs
 echo 'push'
 cd docs
 git push
