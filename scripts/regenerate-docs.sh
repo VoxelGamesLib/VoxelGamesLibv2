bd9a6eeab89a8753fbeb27d35e3893b2b7aceb53
MiniDigger <mini.digger@live.de>
2018-02-19 17:13:37 +0100

    try a captial O :shrug:

--- scripts/regenerate-docs.sh
+++ scripts/regenerate-docs.sh
@@ -1,5 +1,5 @@
 echo 'wget cli.jar'
-wget https://github.com/VoxelGamesLib/VoxelGamesLibv2/raw/gh-pages/commandline-tools/tools/commandline-tools-1.0-SNAPSHOT.jar -o cli.jar
+wget https://github.com/VoxelGamesLib/VoxelGamesLibv2/raw/gh-pages/commandline-tools/tools/commandline-tools-1.0-SNAPSHOT.jar -O cli.jar
 echo 'run cli.jar'
 java -jar cli.jar -generateDocs -docsFolder docs
 echo 'push'
