b77780c27919baf32eeb181ce84f88e1dbffd59b
MiniDigger <admin@minidigger.me>
2018-02-19 22:53:12 +0100

    readd

--- scripts/regenerate-docs.sh
+++ scripts/regenerate-docs.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
 echo 'wget cli.jar'
+wget https://github.com/VoxelGamesLib/VoxelGamesLibv2/blob/gh-pages/commandline-tools/tools/commandline-tools-1.0-SNAPSHOT-all.jar?raw=true -O cli.jar
+echo 'run cli.jar'
+java -jar cli.jar -generateDocs -docsFolder docs
+echo 'push'
+cd docs
+git push
