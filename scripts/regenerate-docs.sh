f4aff3ab4494701b73a408687159613bd5c781ef
MiniDigger <admin@minidigger.me>
2018-02-20 22:27:46 +0100

    exit with 0...

--- scripts/regenerate-docs.sh
+++ scripts/regenerate-docs.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
  if [ "$RUNDOCS" != "true" ]; then
     echo "skipping docs"
-    exit 1
+    exit 0
 fi
 echo 'wget cli.jar'
 wget https://github.com/VoxelGamesLib/VoxelGamesLibv2/blob/gh-pages/commandline-tools/tools/commandline-tools-1.0-SNAPSHOT-all.jar?raw=true -O cli.jar
