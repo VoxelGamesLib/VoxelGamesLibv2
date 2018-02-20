d9c1c8f9934fff1b3c90a954eaaccb5b7a029a5d
MiniDigger <admin@minidigger.me>
2018-02-20 22:05:50 +0100

    allow to disable docs in travis

--- scripts/regenerate-docs.sh
+++ scripts/regenerate-docs.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+ if [ "$RUNDOCS" = "false" ]; then
+    echo "skipping docs"
+    exit 1
+fi
 echo 'wget cli.jar'
 wget https://github.com/VoxelGamesLib/VoxelGamesLibv2/blob/gh-pages/commandline-tools/tools/commandline-tools-1.0-SNAPSHOT-all.jar?raw=true -O cli.jar
 echo 'run cli.jar'
