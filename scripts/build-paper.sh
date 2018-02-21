1605087ef89d3d019db5e6bb5386d47b9caa4ba3
MiniDigger <admin@minidigger.me>
2018-02-21 21:21:22 +0100

    fix sorting and only build paper when nessasary

--- scripts/build-paper.sh
+++ scripts/build-paper.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 echo "checking for paper"
+FILE="/home/travis/.m2/repository/com/destroystokyo/paper/paper/1.12.2-R0.1-SNAPSHOT/paper-1.12.2-R0.1-SNAPSHOT.jar"
+if [ $FILE does exist ]; then
+   echo "Paper found, exiting"
+   exit 0
+fi
 
 echo "downloading paper"
 git clone --depth 1 https://github.com/PaperMC/Paper.git
