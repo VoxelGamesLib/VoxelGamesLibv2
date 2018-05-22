VoxelGamesLib/VoxelGamesLibv2

commit c613dc688efcc8d34dc4ff23bc1f2e319c483193

Added conversion methods between Vector2D and Vector3D

VoxelGamesLib/src/main/java/com/voxelgameslib/voxelgameslib/math/Vector3D.java
@@ -270,4 +270,15 @@ public class Vector3D implements Cloneable {
     public Location toLocation(@Nonnull String world) {
         return new Location(Bukkit.getWorld(world), x, y, z);
     }
+
+   /**
+    * Converts this vector into a two dimensional vector using this vector's {@link Vector3D#x} and
+    * {@link Vector3D#y} values.
+    *
+    * @return This vector converted to a 2D one.
+    */
+    @Nonnull
+    public Vector2D toVector2D() {
+                return new Vector2D(x, y);
+    }
 }

VoxelGamesLib/src/main/java/com/voxelgameslib/voxelgameslib/math/Vector2D.java
@@ -229,4 +229,26 @@ public class Vector2D implements Cloneable {
     public Location toLocation(@Nonnull String world, double z) {
         return new Location(Bukkit.getWorld(world), x, y, z);
     }
+
+    /**
+     * Converts this vector into a three dimensional vector where z equals 0.
+     *
+     * @return This vector converted to a 3D one.
+     */
+    @Nonnull
+    public Vector3D toVector3D() {
+        return this.toVector3D(0);
+    }
+
+
+    /**
+     * Converts this vector into a three dimensional vector.
+     *
+     * @param z The z value to be used by the new vector.
+     * @return This vector converted to a 3D one.
+     */
+    @Nonnull
+    public Vector3D toVector3D(double z) {
+        return new Vector3D(x, y, z);
+    }
 }

commit a4f38752a39ad448366808f41a7daec1b43dfecf

fix whitespace issue introduced by #147

VoxelGamesLib/src/main/java/com/voxelgameslib/voxelgameslib/math/Vector3D.java
@@ -279,6 +279,6 @@ public class Vector3D implements Cloneable {
     */
     @Nonnull
     public Vector2D toVector2D() {
-                return new Vector2D(x, y);
+        return new Vector2D(x, y);
     }
 }