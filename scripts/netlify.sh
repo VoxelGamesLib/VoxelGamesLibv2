b91266892d2d2ec8c43b29c8751b0c7e524d7361
MiniDigger <admin@minidigger.me>
2019-11-24 10:50:24 +0100

    try to use netlify

---
+++ scripts/netlify.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+echo "Setup git"
+git config --global user.email "vglbot@minidigger.me"
+git config --global user.name "VoxelGamesLibBot"
+
+echo "Cloning Submodules"
+git submodule update --init --recursive
+
+echo "Running maven"
+mvn -B package
+
+echo "copy stuff to deploy"
+
+declare -a paths=("VoxelGamesLib/" "games/1vs1/" "games/Deathmatch/" "games/Hub/" "tools/ChatMenuAPI/" "tools/commandline-tools/" "tools/KVGL/" "tools/dependencies/")
+declare -a names=("VGL" "1vs1" "Deathmatch" "Hub" "ChatMenuAPI" "commandline-tools" "KVGL" "dependencies")
+
+arraylength=${#paths[@]}
+
+for (( i=1; i<${arraylength}+1; i++ ));
+do
+  echo $i " / " ${arraylength}
+  cp -R ${paths[$i-1]}build/dependencyUpdates/. deploy-stuff/${names[$i-1]}
+  cp -R ${paths[$i-1]}build/docs/javadoc/. deploy-stuff/${names[$i-1]}/javadoc
+  cp -R ${paths[$i-1]}build/reports/. deploy-stuff/${names[$i-1]}
+  cp -R ${paths[$i-1]}build/libs/. deploy-stuff/${names[$i-1]}
+done
+
+echo "create index"
+sudo pip install mako
+python scripts/make_index.py --header "VGL Deployments" deploy-stuff
