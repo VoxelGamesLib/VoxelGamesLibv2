cd835195b315a4217d8592034a57868d18fb7bfc
MiniDigger <admin@minidigger.me>
2019-11-24 11:17:34 +0100

    fix wrapper path

--- scripts/netlify.sh
+++ scripts/netlify.sh
@@ -8,9 +8,11 @@ echo "Cloning Submodules"
 git submodule update --init --recursive
 
 echo "Running maven"
-../mvnw -B package
+./mvnw -B package
 
 echo "copy stuff to deploy"
+
+mkdir deploy-stuff
 
 declare -a paths=("VoxelGamesLib/" "games/1vs1/" "games/Deathmatch/" "games/Hub/" "tools/ChatMenuAPI/" "tools/commandline-tools/" "tools/KVGL/" "tools/dependencies/")
 declare -a names=("VGL" "1vs1" "Deathmatch" "Hub" "ChatMenuAPI" "commandline-tools" "KVGL" "dependencies")
@@ -20,10 +22,10 @@ arraylength=${#paths[@]}
 for (( i=1; i<${arraylength}+1; i++ ));
 do
   echo $i " / " ${arraylength}
-  cp -R ${paths[$i-1]}build/dependencyUpdates/. deploy-stuff/${names[$i-1]}
-  cp -R ${paths[$i-1]}build/docs/javadoc/. deploy-stuff/${names[$i-1]}/javadoc
-  cp -R ${paths[$i-1]}build/reports/. deploy-stuff/${names[$i-1]}
-  cp -R ${paths[$i-1]}build/libs/. deploy-stuff/${names[$i-1]}
+  #cp -R ${paths[$i-1]}build/dependencyUpdates/. deploy-stuff/${names[$i-1]}
+  cp -R ${paths[$i-1]}build/apidocs/. deploy-stuff/${names[$i-1]}/javadoc
+  #cp -R ${paths[$i-1]}build/reports/. deploy-stuff/${names[$i-1]}
+  #cp -R ${paths[$i-1]}build/libs/. deploy-stuff/${names[$i-1]}
 done
 
 echo "create index"
